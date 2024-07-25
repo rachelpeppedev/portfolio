2c18b2fbc258727f35a9cb0eff4a059012e4a38e
Rachel Peppe <rapeppe@hotmail.com>
2024-07-25 14:25:51 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/rachelpeppedev.png" />
-    <Titulo fontSize={20}>Rachel Peppe</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Rachel Peppe</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        rachelpeppedev
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor full stack
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
